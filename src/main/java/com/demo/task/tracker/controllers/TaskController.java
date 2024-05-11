9e3a26d53424f594d6844c87e31107e92527336f
Leopoldo Ramirez <rmontesleo@gmail.com>
2024-05-10 18:37:31 -0600

    backup project. pending solve issues

--- src/main/java/com/demo/task/tracker/controllers/TaskController.java
+++ src/main/java/com/demo/task/tracker/controllers/TaskController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.demo.task.tracker.models.Task;
+import com.demo.task.tracker.dto.TaskDto;
 import com.demo.task.tracker.services.TaskService;
 
 import lombok.AllArgsConstructor;
@@ -28,14 +28,12 @@ public class TaskController {
     private final TaskService taskService;
 
     @GetMapping()
-    public ResponseEntity<List<Task>> getAllTask(){
-        log.debug("### Returning all task...");
+    public ResponseEntity<List<TaskDto>> getAllTask(){
         return  ResponseEntity.ok( taskService.getAllTask() );
     }
 
     @PostMapping()
-    public ResponseEntity<Task> postTask(  @RequestBody Task newTask ){
-        log.debug("New task is {}", newTask);
+    public ResponseEntity<TaskDto> postTask(  @RequestBody TaskDto newTask ){
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body( taskService.saveTask(newTask)  );
@@ -43,41 +41,29 @@ public class TaskController {
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<Task> getTaskById( @PathVariable Long id ){
-        log.debug("Try to Find task {}", id );
-        return taskService.findTaskById(id)
-                    .map( task -> ResponseEntity.ok(task) )
-                    .orElse( ResponseEntity.status(HttpStatus.NOT_FOUND).build() );
+    public ResponseEntity<TaskDto> findTaskById( @PathVariable Long id ){
+        return taskService.findTaskDtoById(id)
+            .map( task -> ResponseEntity.status(HttpStatus.OK).body(task)  )
+            .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build() );
     }
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTask( @PathVariable Long id ){
-        log.debug("Trying to delete task {}", id);
         if(  taskService.deleteTask(id) ){
-            log.debug("Task with id {} , was delted", id);
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
-        log.debug("Task with id {} , not found", id);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
 
     @PutMapping( "/{id}" )
-    public ResponseEntity<Task> putTask( 
+    public ResponseEntity<TaskDto> putTask( 
         @PathVariable Long id, 
-        @RequestBody Task updatedTask ){
-            log.debug("Updating task with id {} and values {}",  id, updatedTask );
-            
+        @RequestBody TaskDto updatedTask ){        
             return taskService.updateTask(id, updatedTask)
-                        .map( task -> ResponseEntity.ok(task) )
+                        .map( ResponseEntity::ok )
                         .orElse( ResponseEntity.notFound().build() );
-    }
-
-
-    @GetMapping("/fail")
-    public ResponseEntity<Void> failByGet() throws Exception{ 
-        log.debug("Its goint to fail");       
-        throw new Exception("Fail");                
     }
     
 }
